e07b86a798fc31f289dd810eb20d3f365b9ff8c9
joySarkar8 <sarkarjoy498@gmail.com>
2022-10-27 19:45:58 +0600

    question and answers json file added also required

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const questionAnswer = require('./data/questionAndAnswers.json');
 
 app.get('/', (req, res) => {
     res.send('api is running by joy')
@@ -20,6 +21,10 @@ app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const courseDetails = courses.find(course => course.id === id);
     res.send(courseDetails)
+});
+
+app.get('/blog', (req, res) => {
+    res.send(questionAnswer);
 });
 
 
